16bcff72d623fa673e66f5815b8b34ecdbd82f4b
HossamKhir <h.khair95@outlook.com>
2022-12-21 18:36:13 +0000

    Save progress: 840.1

--- insert_data.sh
+++ insert_data.sh
@@ -20,9 +20,10 @@ do
                 echo "Inserted into majors, $MAJOR";
             fi
             # get new major_id
-        MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'");
+            MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'");
         fi
         # get course_id
+        COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'");
         # if not found
             # insert course
             # get new course_id
